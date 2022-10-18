381d8de4b0a6453cadc66461b34a874cda33b25d
Alexandr <jwchaganav@gmail.com>
2022-10-18 10:42:17 +0400

    End

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// На вход 3х значное число и на выходе показывает вторую цифру числа
+Console.Write("Введите 3х значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int first = num/100*10;
+int second = num/10-first;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine($"Вторая цифра числа: {second}");
